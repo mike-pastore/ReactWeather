b2331f015960b37d8ae3dc5c9adbc4cf6797bfcb
Mike Pastore <mike.t.pastore@gmail.com>
2016-08-13 11:54:31 -0500

    Add info to About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -13,8 +13,20 @@ var React = require('react');
 var About = () => {
 	return (
 		<div>
-			<h3>About</h3>
-			<p>Welcome to the about page!</p>
+			<h1 className="text-center">About</h1>
+			<p>Thanks for checking out this page! I'm learning React to create more component-based web apps.</p>
+			<p>Some areas of interest:</p>
+			<ul>
+				<li>
+					<a href="https://www.udemy.com/the-complete-react-web-app-developer-course">The Complete Reach Web App Developer Course</a> - Online class where I learned to make this
+				</li>
+				<li>
+					<a href="http://openweathermap.org/api">OpenWeatherMap.org's API</a> - All weather data comes from there
+				</li>
+				<li>
+					<a href="https://github.com/mike-pastore/ReactWeather">Github repo</a> for this app
+				</li>
+			</ul>
 		</div>
 	);
 }
