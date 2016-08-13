cdc5654486e5fd9345bad991bc434d88fe44e7cb
Mike Pastore <mike.t.pastore@gmail.com>
2016-08-13 12:04:11 -0500

    Add styles to Weather page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -14,17 +14,17 @@ var About = () => {
 	return (
 		<div>
 			<h1 className="text-center">About</h1>
-			<p>Thanks for checking out this page! I'm learning React to create more component-based web apps.</p>
+			<p>Thanks for checking out this page! I'm learning React to create more component-based web apps for easier scalability, maintenance, and overall organization.</p>
 			<p>Some areas of interest:</p>
 			<ul>
 				<li>
-					<a href="https://www.udemy.com/the-complete-react-web-app-developer-course">The Complete Reach Web App Developer Course</a> - Online class where I learned to make this
+					<a href="https://www.udemy.com/the-complete-react-web-app-developer-course" target="_blank">The Complete Reach Web App Developer Course</a> - Online class where I learned to make this
 				</li>
 				<li>
-					<a href="http://openweathermap.org/api">OpenWeatherMap.org's API</a> - All weather data comes from there
+					<a href="http://openweathermap.org/api" target="_blank">OpenWeatherMap.org's API</a> - All weather data comes from there
 				</li>
 				<li>
-					<a href="https://github.com/mike-pastore/ReactWeather">Github repo</a> for this app
+					<a href="https://github.com/mike-pastore/ReactWeather" target="_blank">Github repo</a> for this app
 				</li>
 			</ul>
 		</div>
