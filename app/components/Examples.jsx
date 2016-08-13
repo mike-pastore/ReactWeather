7d42c64317e1aec5dda49aa1427be631b817255d
Mike Pastore <mike.t.pastore@gmail.com>
2016-08-13 11:44:34 -0500

    Add Example page style/examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = () => {
 	return (
 		<div>
-			<h3>Examples</h3>
-			<p>Welcome to the examples page!</p>
+			<h1 className="text-center">Examples</h1>
+			<p>Here are a few example locations to try out:</p>
+			<ol>
+				<li>
+					<Link to="/?Chicago">Chicago, IL</Link>
+				</li>
+				<li>
+					<Link to="/?Providence">Providence, RI</Link>
+				</li>
+			</ol>
 		</div>
 	);
 }
