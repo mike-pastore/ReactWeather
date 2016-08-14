ee823eefdbe228c5dade254cc239bef933b008f3
Mike Pastore <mike.t.pastore@gmail.com>
2016-08-14 13:51:01 -0500

    Fix error modal, DOM and React weren't talking right

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 var ErrorModal = new React.createClass({
 	getDefaultProps: function () {
@@ -11,14 +13,11 @@ var ErrorModal = new React.createClass({
 		message: React.PropTypes.string.isRequired
 	},
 	componentDidMount: function () {
-		var modal = new Foundation.Reveal($('#error-modal'));
-		modal.open();
-	},
-	render: function () {
 		// pull off props to work with
 		var {title, message} = this.props;
-		
-		return (
+
+		// handle all content here, so that React keeps track of DOM changes from Foundation's modal component
+		var modalMarkup = (
 			<div id="error-modal" className="reveal tiny text-center" data-reveal="">
 				<h4>{title}</h4>
 				<p>{message}</p>
@@ -27,6 +26,20 @@ var ErrorModal = new React.createClass({
 						Okay
 					</button>
 				</p>
+			</div>
+		);
+
+		// mount modalMarkup DOM into a string, to pass into Foundation's modal (so React is keeping track of all of this)
+		var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+		// find DOM node where this ErrorModal.jsx modal lives, inject string version of modalMarkup
+		$(ReactDOM.findDOMNode(this)).html($modal);
+
+		var modal = new Foundation.Reveal($('#error-modal'));
+		modal.open();
+	},
+	render: function () {	
+		return (
+			<div>
 			</div>
 		);
 	}
